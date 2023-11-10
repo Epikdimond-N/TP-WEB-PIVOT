81d66041d48ca8628fdb6bea9aaba81c3e07156f
Epikdimond-N <nathanael.pivot@ynov.com>
2023-11-10 15:28:06 +0100

    challenge 3

--- main.go
+++ main.go
@@ -9,13 +9,22 @@ import (
 
 var c int = 0
 
+type User struct {
+	Nom    string
+	Prenom string
+	Anniv  string
+	Sexe   string
+}
+
+var data = User{}
+
 func main() {
 	temp, err := template.ParseGlob("./temp/*.html")
 	if err != nil {
 		fmt.Println(fmt.Sprintf("ERREUR => %s", err.Error()))
 		return
 	}
-	type User struct {
+	type Mentor struct {
 		FirstName string
 		LastName  string
 		Age       int
@@ -28,11 +37,11 @@ func main() {
 		Niveau   int
 		Nombre   int
 		Liste    string
-		Etudiant []User
+		Etudiant []Mentor
 	}
 
 	http.HandleFunc("/promo", func(w http.ResponseWriter, r *http.Request) {
-		Users := []User{{"Cyril", "RODRIGUES", 22, true},
+		Users := []Mentor{{"Cyril", "RODRIGUES", 22, true},
 			{"Kheir-eddine", "MEDERREG", 22, false},
 			{"Alan", "PHILIPIERT", 26, true}}
 		dataPage := PageVariable{"Information de la promotion", "Mentor'ac", "informatique", 5, 3, "Liste des Etudiants", Users}
@@ -45,7 +54,6 @@ func main() {
 	}
 	http.HandleFunc("/change", func(w http.ResponseWriter, r *http.Request) {
 		c++
-
 		var laverite bool
 		if c%2 == 0 {
 			laverite = false
@@ -54,6 +62,24 @@ func main() {
 		}
 		var dataPage = Display{c, laverite}
 		temp.ExecuteTemplate(w, "change", dataPage)
+	})
+
+	http.HandleFunc("/user/init", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "init", nil)
+	})
+
+	http.HandleFunc("/user/treatment", func(w http.ResponseWriter, r *http.Request) {
+		data = User{
+			r.FormValue("nom"),
+			r.FormValue("prenom"),
+			r.FormValue("date"),
+			r.FormValue("sexe")}
+		fmt.Println(data)
+		http.Redirect(w, r, "/user/display", http.StatusMovedPermanently)
+	})
+
+	http.HandleFunc("/user/display", func(w http.ResponseWriter, r *http.Request) {
+		temp.ExecuteTemplate(w, "display", data)
 	})
 
 	rootDoc, _ := os.Getwd()
